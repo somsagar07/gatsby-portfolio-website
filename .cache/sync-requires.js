475b22ed6f7d93ce521bb5a4710da3e71fb4014f
Som Sagar <somsagar2010@gmail.com>
2021-01-22 22:40:43 +0530

    upload

---
+++ .cache/sync-requires.js
@@ -0,0 +1,11 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\Users\\Sagar\\Desktop\\gatsby web\\gatsby-website\\.cache\\dev-404-page.js"))),
+  "component---node-modules-lekoarts-gatsby-theme-cara-src-templates-cara-tsx": hot(preferDefault(require("C:\\Users\\Sagar\\Desktop\\gatsby web\\gatsby-website\\node_modules\\@lekoarts\\gatsby-theme-cara\\src\\templates\\cara.tsx")))
+}
+
